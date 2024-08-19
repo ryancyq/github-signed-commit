5d679bf140f5887f4ae7d323977ce7dd2f213662
Ryan Chang <ryancyq@gmail.com>
2024-08-19 17:09:12 +0800

    development setup

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/.*',
+      '**/node_modules/*',
+      'build/*',
+      'package/*',
+
+      // Ignore files for PNPM, NPM and YARN
+      '**/pnpm-lock.yaml',
+      '**/package-lock.json',
+      '**/yarn.lock',
+
+      // Ignore config type check
+      '**/*.config.js',
+      '**/*.config.ts'
+    ]
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      }
+    }
+  }
+);
