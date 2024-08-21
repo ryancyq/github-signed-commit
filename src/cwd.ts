db8a2f7e9314bb20e277055199e51f02e11edca3
Ryan Chang <ryancyq@gmail.com>
2024-08-21 18:01:47 +0800

    update cwd.ts to align with input for string return type

--- src/cwd.ts
+++ src/cwd.ts
@@ -1,10 +1,17 @@
+import * as core from '@actions/core'
 import { getInput } from './input'
 
 export function getCwd() {
-  return (
-    getInput('workspace', { default: process.env.GITHUB_WORKSPACE }) ??
-    process.cwd()
-  )
+  const workspace = getInput('workspace', {
+    default: process.env.GITHUB_WORKSPACE,
+  })
+  if (workspace) {
+    core.debug(`cwd: ${workspace}`)
+    return workspace
+  }
+  const current = process.cwd()
+  core.debug(`cwd: ${current}`)
+  return current
 }
 
 const cwd = getCwd()
