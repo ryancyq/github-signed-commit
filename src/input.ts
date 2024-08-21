153c344afb791fe825144a29893dcb0ac227fe57
Ryan Chang <ryancyq@gmail.com>
2024-08-21 18:01:24 +0800

    fix input to align with core.getInput always return string type

--- src/input.ts
+++ src/input.ts
@@ -1,13 +1,10 @@
 import * as core from '@actions/core'
 
 export interface InputOptions extends core.InputOptions {
-  default?: string | undefined
+  default?: string
 }
 
-export function getInput(
-  name: string,
-  options: InputOptions = {}
-): string | undefined {
+export function getInput(name: string, options: InputOptions = {}): string {
   const value = core.getInput(name, options)
 
   if (!value && options.default) {
