425f203a536b5be66c87849cf0d164f50d8ae622
Ryan Chang <ryancyq@gmail.com>
2024-08-23 03:53:47 +0800

    allow utils input to defer required check after default value evaluation

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -5,12 +5,16 @@ export interface InputOptions extends core.InputOptions {
 }
 
 export function getInput(name: string, options: InputOptions = {}): string {
-  const value = core.getInput(name, options)
+  const required = options.required ?? false
+  const value = core.getInput(name, Object.assign(options, { required: false }))
 
   if (!value && options.default) {
     core.debug(`input: ${name}=${options.default}`)
     return options.default
   }
+
+  if (!value && required)
+    throw new Error(`Input required and not supplied: ${name}`)
 
   core.debug(`input: ${name}=${value}`)
   return value
