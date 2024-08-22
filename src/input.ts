e0f09eb0b3d46782956b9527a1cd0de1ff192ce7
Ryan Chang <ryancyq@gmail.com>
2024-08-22 16:05:31 +0800

    add context to debug logging

--- src/input.ts
+++ src/input.ts
@@ -8,10 +8,10 @@ export function getInput(name: string, options: InputOptions = {}): string {
   const value = core.getInput(name, options)
 
   if (!value && options.default) {
-    core.debug(`${name}: ${options.default}`)
+    core.debug(`input: ${name}=${options.default}`)
     return options.default
   }
 
-  core.debug(`${name}: ${value}`)
+  core.debug(`input: ${name}=${value}`)
   return value
 }
