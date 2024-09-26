bc78a84c2af40a765bcfedd47b1450df026605a4
Ryan Chang <ryancyq@gmail.com>
2024-09-27 03:58:50 +0800

    test: update graphql test for create commit/tag

--- __tests__/github/graphql.test.ts
+++ __tests__/github/graphql.test.ts
@@ -14,7 +14,11 @@ import {
 } from '@octokit/graphql-schema'
 import * as client from '../../src/github/client'
 import * as blob from '../../src/blob'
-import { getRepository, createCommitOrTag } from '../../src/github/graphql'
+import {
+  getRepository,
+  createCommitOnBranch,
+  createTagOnCommit,
+} from '../../src/github/graphql'
 
 describe('GitHub API', () => {
   beforeEach(() => {
@@ -154,11 +158,8 @@ describe('GitHub API', () => {
     })
   })
 
-  describe('createCommitOrTag', () => {
+  describe('createCommitOnBranch', () => {
     it('should create a commit on the given branch', async () => {
-      jest.spyOn(core, 'getInput').mockImplementation((name, options) => {
-        return name === 'commit-message' ? 'fake commit message' : ''
-      })
       const clientMock = jest.spyOn(client, 'graphqlClient').mockReturnValue(
         graphql.defaults({
           request: {
@@ -167,6 +168,8 @@ describe('GitHub API', () => {
                 createCommitOnBranch: {
                   commit: {
                     oid: 'commit-id',
+                    message: 'fake commit message',
+                    committedDate: '2024-08-19T04:53:47Z',
                     __typename: 'Commit',
                   },
                   __typename: 'CreateCommitOnBranchPayload',
@@ -182,16 +185,22 @@ describe('GitHub API', () => {
       const currentCommit = {} as Commit
       const fileChanges = {} as FileChanges
       await expect(
-        createCommitOrTag(currentCommit, { branch, fileChanges })
-      ).resolves.toHaveProperty('createCommitOnBranch.commit.oid', 'commit-id')
+        createCommitOnBranch(
+          currentCommit,
+          'fake commit message',
+          branch,
+          fileChanges
+        )
+      ).resolves.toHaveProperty('commit.oid', 'commit-id')
 
       expect(clientMock).toHaveBeenCalled()
       expect(debugMock).toHaveBeenCalledWith(
         expect.stringMatching(
-          /Request\[createCommitOrTag\] successful, query: [\s\S]*, variables: [\s\S]*, data: [\s\S]*/
+          /Request\[createCommitOnBranch\] successful, query: [\s\S]*, variables: [\s\S]*, data: [\s\S]*/
         )
       )
     })
+
     it('should handle GraphqlResponseError', async () => {
       const clientMock = jest.spyOn(client, 'graphqlClient').mockReturnValue(
         graphql.defaults({
@@ -210,7 +219,12 @@ describe('GitHub API', () => {
       const currentCommit = {} as Commit
       const fileChanges = {} as FileChanges
       await expect(
-        createCommitOrTag(currentCommit, { branch, fileChanges })
+        createCommitOnBranch(
+          currentCommit,
+          'random commit message',
+          branch,
+          fileChanges
+        )
       ).rejects.toThrow('GraphQL error')
 
       expect(clientMock).toHaveBeenCalled()
@@ -219,10 +233,11 @@ describe('GitHub API', () => {
       )
       expect(debugMock).toHaveBeenCalledWith(
         expect.stringMatching(
-          /Request\[createCommitOrTag\] failed, query: [\s\S]*, variables: [\s\S]*, data: [\s\S]*/
+          /Request\[createCommitOnBranch\] failed, query: [\s\S]*, variables: [\s\S]*, data: [\s\S]*/
         )
       )
     })
+
     it('should populate file changes content', async () => {
       const fileAddition: FileAddition = {
         path: 'my_commit.txt',
@@ -261,10 +276,12 @@ describe('GitHub API', () => {
                     'my-branch'
                   )
                   expect(body.variables).toHaveProperty(
-                    'commitInput.expectedHeadOid'
-                  )
-                  expect(body.variables.commitInput.expectedHeadOid).toContain(
+                    'commitInput.expectedHeadOid',
                     'MyOid'
+                  )
+                  expect(body.variables).toHaveProperty(
+                    'commitInput.message.headline',
+                    'new file content'
                   )
                   expect(body.variables).toHaveProperty(
                     'commitInput.fileChanges.additions'
@@ -285,10 +302,121 @@ describe('GitHub API', () => {
         branchName: 'my-branch',
       } as CommittableBranch
       const currentCommit = { oid: 'MyOid' } as Commit
-      const result = await createCommitOrTag(currentCommit, {
+      const result = await createCommitOnBranch(
+        currentCommit,
+        'new file content',
         branch,
-        fileChanges,
-      })
+        fileChanges
+      )
+      expect(clientMock).toHaveBeenCalled()
+    })
+  })
+
+  describe('createTagOnCommit', () => {
+    it('should create a tag on the given commit', async () => {
+      const clientMock = jest.spyOn(client, 'graphqlClient').mockReturnValue(
+        graphql.defaults({
+          request: {
+            fetch: fetchMock.sandbox().post('https://api.github.com/graphql', {
+              data: {
+                createRef: {
+                  ref: {
+                    name: 'refs/tags/faked-tag',
+                    __typename: 'Ref',
+                  },
+                  __typename: 'CreateRefPayload',
+                },
+              },
+            }),
+          },
+        })
+      )
+      const debugMock = jest.spyOn(core, 'debug').mockReturnValue()
+
+      const currentCommit = {} as Commit
+      await expect(
+        createTagOnCommit(currentCommit, 'fake-tag', 'my-repository-id')
+      ).resolves.toHaveProperty('ref.name', 'refs/tags/faked-tag')
+
+      expect(clientMock).toHaveBeenCalled()
+      expect(debugMock).toHaveBeenCalledWith(
+        expect.stringMatching(
+          /Request\[createTagOnCommit\] successful, query: [\s\S]*, variables: [\s\S]*, data: [\s\S]*/
+        )
+      )
+    })
+
+    it('should handle GraphqlResponseError', async () => {
+      const clientMock = jest.spyOn(client, 'graphqlClient').mockReturnValue(
+        graphql.defaults({
+          request: {
+            fetch: fetchMock.sandbox().post('https://api.github.com/graphql', {
+              errors: [{ message: 'GraphQL error' }],
+              data: null,
+            }),
+          },
+        })
+      )
+      const errorMock = jest.spyOn(core, 'error').mockReturnValue()
+      const debugMock = jest.spyOn(core, 'debug').mockReturnValue()
+
+      const currentCommit = {} as Commit
+      await expect(
+        createTagOnCommit(
+          currentCommit,
+          'fake-another-tag',
+          'another-repository-id'
+        )
+      ).rejects.toThrow('GraphQL error')
+
+      expect(clientMock).toHaveBeenCalled()
+      expect(errorMock).toHaveBeenCalledWith(
+        'Request failed due to following response errors:\n - GraphQL error'
+      )
+      expect(debugMock).toHaveBeenCalledWith(
+        expect.stringMatching(
+          /Request\[createTagOnCommit\] failed, query: [\s\S]*, variables: [\s\S]*, data: [\s\S]*/
+        )
+      )
+    })
+
+    it('should populate tag content', async () => {
+      const clientMock = jest.spyOn(client, 'graphqlClient').mockReturnValue(
+        graphql.defaults({
+          request: {
+            fetch: fetchMock
+              .sandbox()
+              .post(
+                'https://api.github.com/graphql',
+                (_url, options: RequestOptions) => {
+                  const body = JSON.parse(options.body)
+                  expect(body.query).toEqual(
+                    expect.stringMatching(/mutation(.+CreateRefInput)/)
+                  )
+
+                  expect(body.variables).toHaveProperty(
+                    'tagInput.repositoryId',
+                    'my-repo-id'
+                  )
+                  expect(body.variables).toHaveProperty(
+                    'tagInput.name',
+                    'refs/tags/my-tag'
+                  )
+                  expect(body.variables).toHaveProperty('tagInput.oid', 'MyOid')
+
+                  return { data: {} }
+                }
+              ),
+          },
+        })
+      )
+
+      const currentCommit = { oid: 'MyOid' } as Commit
+      const result = await createTagOnCommit(
+        currentCommit,
+        'my-tag',
+        'my-repo-id'
+      )
       expect(clientMock).toHaveBeenCalled()
     })
   })
