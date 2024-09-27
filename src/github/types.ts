b4430a04623f4769544a881d4d2268b3bb27e17d
Ryan Chang <ryancyq@gmail.com>
2024-09-27 14:06:00 +0800

    burn: remove commit type check

--- src/github/types.ts
+++ src/github/types.ts
@@ -1,15 +1,9 @@
 import {
-  Commit,
   CommitHistoryConnection,
   Maybe,
   Ref,
   Repository,
 } from '@octokit/graphql-schema'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function isCommit(obj: any): obj is Commit {
-  return !!obj && 'oid' in obj && 'message' in obj && 'committedDate' in obj
-}
 
 export type RefWithCommitHistory = Omit<Ref, 'target'> & {
   target: {
