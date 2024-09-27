ed0d4a0e02c786ffb1b5ec53929fcab28a957fcf
Ryan Chang <ryancyq@gmail.com>
2024-09-27 11:01:52 +0800

    fix: extract repo, owner, branch resolution logic

---
+++ __tests__/github/repo.test.ts
@@ -0,0 +1,35 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { describe, jest, beforeEach, it, expect } from '@jest/globals'
+import { getContext } from '../../src/github/repo'
+
+describe('getContext', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('extract owner and repo', () => {
+    jest
+      .spyOn(github.context, 'repo', 'get')
+      .mockReturnValue({ repo: 'my-repo', owner: 'my-user' })
+    jest.replaceProperty(github.context, 'ref', 'refs/heads/main')
+    const context = getContext()
+    expect(context).toHaveProperty('owner', 'my-user')
+    expect(context).toHaveProperty('repo', 'my-repo')
+  })
+
+  describe('branch', () => {
+    it('resolves head ref', () => {
+      jest.replaceProperty(github.context, 'ref', 'refs/heads/default-branch')
+      expect(getContext()).toHaveProperty('branch', 'default-branch')
+    })
+
+    it('resolves pull request ref', async () => {
+      jest.replaceProperty(github.context, 'ref', 'refs/pull/123/merge')
+      jest.replaceProperty(github.context, 'payload', {
+        pull_request: { number: 123, head: { ref: 'my-branch' } },
+      })
+      expect(getContext()).toHaveProperty('branch', 'my-branch')
+    })
+  })
+})
---
+++ src/github/repo.ts
@@ -0,0 +1,21 @@
+import * as github from '@actions/github'
+
+function resolveCurrentBranch(ref: string): string {
+  if (ref.startsWith('refs/heads/')) {
+    return ref.replace(/refs\/heads\//g, '')
+  } else if (ref.startsWith('refs/pull/')) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+    return github.context.payload.pull_request?.head?.ref ?? ''
+  }
+
+  throw new Error(`Unsupported ref: ${ref}`)
+}
+
+export function getContext() {
+  const { ref, repo } = github.context
+  return {
+    owner: repo.owner,
+    repo: repo.repo,
+    branch: resolveCurrentBranch(ref),
+  }
+}
