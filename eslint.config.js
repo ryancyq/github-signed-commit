30f39788d2ac6d4e7e22d49604ca009e2ee4fcc4
Ryan Chang <ryancyq@gmail.com>
2026-07-02 17:43:14 +0800

    chore: update eslint 10

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,29 @@
 import eslint from '@eslint/js'
-import globals from 'globals'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default tseslint.config(
-  {
-    ignores: [
-      '**/.*',
-      '**/node_modules/*',
-      'dist/*',
+export default defineConfig(
+  globalIgnores([
+    '**/.*',
+    '**/node_modules/*',
+    'dist/*',
 
-      // Ignore files for PNPM, NPM and YARN
-      '**/pnpm-lock.yaml',
-      '**/package-lock.json',
-      '**/yarn.lock',
+    // Ignore files for PNPM, NPM and YARN
+    '**/pnpm-lock.yaml',
+    '**/package-lock.json',
+    '**/yarn.lock',
 
-      // Ignore config type check
-      '**/*.config.js',
-      '**/*.config.ts',
-    ],
-  },
+    // Ignore config type check
+    '**/*.config.js',
+    '**/*.config.ts',
+  ]),
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   eslintConfigPrettier,
   {
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-    },
     languageOptions: {
-      parser: tseslint.parser,
       parserOptions: {
         project: true,
         tsconfigRootDir: import.meta.dirname,
